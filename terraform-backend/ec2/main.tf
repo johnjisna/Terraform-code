b4930ab77599d2e8a2ca8314b6f444419840fd1d
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-10 19:26:41 +0530

    Update main.tf

--- terraform-backend/ec2/main.tf
+++ terraform-backend/ec2/main.tf
@@ -7,18 +7,3 @@ resource "aws_instance" "web" {
     Name        = var.instance_name
     Environment = var.environment
   }
-
-  user_data = <<EOT
-#!/bin/bash
-# Install Docker
-sudo apt-get update
-sudo apt-get install -y docker.io
-
-# Login to ECR
-$(aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.ecr_repository_url})
-
-# Pull and run the Docker container
-docker pull ${var.ecr_repository_url}:latest
-docker run -d --name my-app -p 80:80 ${var.ecr_repository_url}:latest
-EOT
-}
