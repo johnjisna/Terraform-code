6181e5d19a6e2b3b12adc8ce3504e2fc729ef565
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-11 10:27:16 +0530

    Update main.tf

--- terraform-backend/iam/main.tf
+++ terraform-backend/iam/main.tf
@@ -2,7 +2,6 @@ resource "aws_iam_user" "user" {
   name = var.iam_user_name
 }
 
-
 resource "aws_iam_policy" "policies" {
   for_each    = var.iam_policies
   name        = each.key
@@ -10,22 +9,23 @@ resource "aws_iam_policy" "policies" {
   policy      = file(each.value)
 }
 
-
 resource "aws_iam_user_policy_attachment" "policy_attachments" {
   for_each   = aws_iam_policy.policies
   user       = aws_iam_user.user.name
   policy_arn = each.value.arn
 }
 
-resource "aws_iam_role" "ecr_read" {
-  name = var.iam_role_name
+
+resource "aws_iam_role" "role" {
+  count = var.create_role ? 1 : 0
+  name  = var.iam_role_name
 
   assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
+    Version   = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow"
-        Action    = "sts:AssumeRole"
+        Effect    = "Allow",
+        Action    = "sts:AssumeRole",
         Principal = {
           Service = var.trusted_services
         }
@@ -34,9 +34,9 @@ resource "aws_iam_role" "ecr_read" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "policy_attachments" {
-  for_each   = aws_iam_policy.policies
-  role       = aws_iam_role.ecr_read.name
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachments" {
+  for_each = var.create_role ? aws_iam_policy.policies : {}
+  role     = aws_iam_role.role[0].name
   policy_arn = each.value.arn
 }
-
