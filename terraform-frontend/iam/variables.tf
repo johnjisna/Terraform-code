f95384a65c64c197488d298a1fc173882a626d75
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-02-11 10:25:18 +0530

    Update variables.tf

--- terraform-frontend/iam/variables.tf
+++ terraform-frontend/iam/variables.tf
@@ -1,9 +1,27 @@
 variable "iam_user_name" {
-  description = "The name of the IAM user"
+  description = "Name of the IAM user."
   type        = string
 }
 
 variable "iam_policies" {
-  description = "Map of policy names to their JSON file paths for the IAM user"
+  description = "Map of policy names to file paths for the policy documents."
   type        = map(string)
+}
+
+variable "create_role" {
+  description = "Set to true to create an IAM role and attach policies to it."
+  type        = bool
+  default     = false
+}
+
+variable "iam_role_name" {
+  description = "Name of the IAM role to create when create_role is true."
+  type        = string
+  default     = ""
+}
+
+variable "trusted_services" {
+  description = "List of trusted service principals for the IAM role."
+  type        = list(string)
+  default     = []
 }
